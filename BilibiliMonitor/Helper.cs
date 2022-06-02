fa7e32c1ccba9d94d03e0ef918605d31a6ee5b7f
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-02 17:41:42 +0800

    杂项

--- BilibiliMonitor/Helper.cs
+++ BilibiliMonitor/Helper.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +8,29 @@ namespace BilibiliMonitor
 {
     public static class Helper
     {
-        
+        public static long TimeStamp => (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        public static async Task<string> Get(string url)
+        {
+            using var http = new HttpClient();
+            var r = await http.GetAsync(url);
+            return await r.Content.ReadAsStringAsync();
+        }
+        public static async Task<string> Post(string url, object payload)
+        {
+            using var http = new HttpClient();
+            var r = await http.PostAsync(url, new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
+            return await r.Content.ReadAsStringAsync();
+        }
+        public static bool CompareNumString(string a, string b)
+        {
+            if (a.Length != b.Length)
+                return a.Length>b.Length;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    return a[i] > b[i];
+            }
+            return false;
+        }
     }
 }
