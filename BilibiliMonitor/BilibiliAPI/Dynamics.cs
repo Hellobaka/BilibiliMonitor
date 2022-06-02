a11a926bc75fde8f71efb08aa2e0516cebf786c3
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-02 17:41:50 +0800

    拉取动态列表

--- BilibiliMonitor/BilibiliAPI/Dynamics.cs
+++ BilibiliMonitor/BilibiliAPI/Dynamics.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BilibiliMonitor.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace BilibiliMonitor.BilibiliAPI
 {
-    internal class Dynamics
+    public class Dynamics
     {
+        private static string BaseUrl = "https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid={0}";
+        public int UID { get; set; }
+        public string LastDynamicID { get; set; }
+        public List<DynamicModel.Item> DynamicList { get; set; }
+        public bool FetchDynamicList()
+        {
+            string url = string.Format(BaseUrl, UID);
+            var json = JsonConvert.DeserializeObject<DynamicModel.Main>(Helper.Get(url).Result);
+            if (json.code == 0)
+            {
+                DynamicList = json.data.items.ToList();
+                if (DynamicList.Count > 0) LastDynamicID = DynamicList[0].id_str;
+                for (int i = 1; i < DynamicList.Count; i++)
+                {                    
+                    if(!Helper.CompareNumString(LastDynamicID, DynamicList[i].id_str))
+                    {
+                        LastDynamicID = DynamicList[i].id_str;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
