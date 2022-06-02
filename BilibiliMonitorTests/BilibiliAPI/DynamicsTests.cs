617c90df895afcf7fad15a1ed65721c4710e5d8a
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-02 18:26:40 +0800

    单元测试

---
+++ BilibiliMonitorTests/BilibiliAPI/DynamicsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilibiliMonitor.BilibiliAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilibiliMonitor.BilibiliAPI.Tests
+{
+    [TestClass()]
+    public class DynamicsTests
+    {
+        [TestMethod()]
+        public void FetchDynamicListTest()
+        {
+            Dynamics dy = new(692283831);
+            dy.FetchDynamicList();
+            Console.WriteLine(dy.LastDynamicID);
+        }
+    }
+}
---
+++ BilibiliMonitorTests/BilibiliAPI/LiveStreamsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilibiliMonitor.BilibiliAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilibiliMonitor.BilibiliAPI.Tests
+{
+    [TestClass()]
+    public class LiveStreamsTests
+    {
+        [TestMethod()]
+        public void FetchLiveStreamTest()
+        {
+            LiveStreams.AddUID(692283831);
+            Console.WriteLine(LiveStreams.LiveStreamData[692283831].live_status);
+        }
+    }
+}
