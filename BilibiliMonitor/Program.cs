45bd4f739caf28fa7dd1d82e2c81ee838716c88c
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-02 16:55:36 +0800

    添加项目文件。

---
+++ BilibiliMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilibiliMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
