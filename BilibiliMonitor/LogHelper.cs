c63937b0e2c829bc7e38a01003ddabf88a3119f5
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-07 23:25:12 +0800

    日志方法

---
+++ BilibiliMonitor/LogHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilibiliMonitor
+{
+    public class Log
+    {
+        
+    }
+    public static class LogHelper
+    {
+        public static void Info(string type, string message, bool status = true)
+        {
+            Console.WriteLine($"{(status?"[+]":"[-]")}[{DateTime.Now:G}][{type}]{message}");
+        }
+    }
+}
