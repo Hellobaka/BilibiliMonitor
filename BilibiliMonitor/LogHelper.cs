572523dad2ed6eb3bcdd762070bd5763d9ec9759
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-10 20:18:27 +0800

    日志方法可自定义

--- BilibiliMonitor/LogHelper.cs
+++ BilibiliMonitor/LogHelper.cs
@@ -12,9 +12,17 @@ namespace BilibiliMonitor
     }
     public static class LogHelper
     {
+        public static Action<string, string, bool> InfoMethod = null;
         public static void Info(string type, string message, bool status = true)
         {
-            Console.WriteLine($"{(status?"[+]":"[-]")}[{DateTime.Now:G}][{type}]{message}");
+            if(InfoMethod == null)
+            {
+                Console.WriteLine($"{(status ? "[+]" : "[-]")}[{DateTime.Now:G}][{type}]{message}");
+            }
+            else
+            {
+                InfoMethod.Invoke(type, message, status);
+            }
         }
     }
 }
