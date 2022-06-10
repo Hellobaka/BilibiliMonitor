cefd416cb3160b6a9b86ba837f76e5618ceb4448
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-10 10:14:47 +0800

    添加直播检查

--- BilibiliMonitor/UpdateChecker.cs
+++ BilibiliMonitor/UpdateChecker.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using BilibiliMonitor.Models;
 
 namespace BilibiliMonitor
 {
@@ -12,10 +13,15 @@ namespace BilibiliMonitor
     {
         public static string BasePath { get; set; } = "";
         public static string PicPath { get; set; } = "";
-        public int DynamicCheckCD { get; set; } = 1;
-        public List<Dynamics> Dynamics { get; set; } = new();
+        public int DynamicCheckCD { get; set; } = 5;
         public static UpdateChecker Instance { get; private set; }
         public bool Enabled { get; set; } = false;
+        public List<Dynamics> Dynamics { get; set; } = new();
+
+        public delegate void DynamicUpdateHandler(DynamicModel.Item item, string picPath);
+        public event DynamicUpdateHandler OnDynamic;
+        public delegate void StreamOpenHandler(LiveStreamsModel.RoomInfo item, string picPath);
+        public event StreamOpenHandler OnStream;
         public UpdateChecker(string basePath, string picPath)
         {
             BasePath = basePath;
@@ -35,8 +41,21 @@ namespace BilibiliMonitor
                                 string pic = dy.DrawImage();
                                 if(string.IsNullOrEmpty(pic) == false)
                                 {
+                                    OnDynamic?.Invoke(dy.LatestDynamic, pic);
                                     LogHelper.Info("动态更新", $"{dy.UserName}的动态有更新，id={dy.LastDynamicID}，路径={pic}");
                                 }
+                            }
+                        }
+
+                        foreach (var uid in LiveStreams.FetchLiveStream())
+                        {
+                            LiveStreams.DownloadPics(uid);
+                            string pic = LiveStreams.DrawLiveStreamPic(uid);
+                            if (string.IsNullOrEmpty(pic) == false)
+                            {
+                                var info = LiveStreams.LiveStreamData[uid];
+                                OnStream?.Invoke(info, pic);
+                                LogHelper.Info("开播", $"{info.uname}开播了，路径={pic}");
                             }
                         }
                     }
@@ -53,6 +72,25 @@ namespace BilibiliMonitor
             var dy = new Dynamics(uid);
             dy.FetchDynamicList();
             Dynamics.Add(dy);
+        }
+
+        public void RemoveDynamic(int uid)
+        {
+            if (Dynamics.Any(x => x.UID == uid))
+            {
+                return;
+            }
+
+            Dynamics.Remove(Dynamics.First(x => x.UID == uid));
+        }
+        public void AddStream(int uid)
+        {
+            LiveStreams.AddUID(uid);
+        }
+
+        public void RemoveStream(int uid)
+        {
+            LiveStreams.RemoveUID(uid);
         }
         public void Start()
         {
