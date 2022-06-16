e7ff28ad5703935f495a4916999d70cb0d065cf6
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-16 09:47:05 +0800

    修复直播

--- BilibiliMonitor/BilibiliAPI/LiveStreams.cs
+++ BilibiliMonitor/BilibiliAPI/LiveStreams.cs
@@ -1,12 +1,7 @@
 ﻿using BilibiliMonitor.Models;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
@@ -17,89 +12,68 @@ using Path = System.IO.Path;
 
 namespace BilibiliMonitor.BilibiliAPI
 {
-    public static class LiveStreams
+    public class LiveStreams
     {
-        private static string BaseURL = "http://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids";
-        public static Dictionary<int, LiveStreamsModel.RoomInfo> LiveStreamData { get; set; } = new();
+        private static string BaseUIDQueryURL = "https://api.live.bilibili.com/live_user/v1/Master/info?uid=";
+        private static string BaseRoomInfoURL = "https://api.live.bilibili.com/room/v1/Room/get_info?room_id=";
+        private static string BasePicURL = "https://api.live.bilibili.com/room/v1/Room/get_info?room_id=";
+        public int UID { get; set; }
+        public int RoomID { get; set; }
+        public string Name { get; set; }
+        public bool Streaming { get => StreamingStatus == 1; }
+        private int StreamingStatus { get; set; }
+        public LiveStreamsModel.RoomInfo RoomInfo { get; set; }
+        public LiveStreamsModel.UserInfo UserInfo { get; set; }
 
-        public static LiveStreamsModel.RoomInfo AddUID(int uid)
+        public LiveStreams(int uid)
         {
-            FetchLiveStream(new List<int> {uid});
-            return LiveStreamData[uid];
+            UID = uid;
+            FetchUserInfo();
         }
-
-        public static void RemoveUID(int uid)
+        public void FetchUserInfo()
         {
-            LiveStreamData.Remove(uid);
-        }
-
-        public static List<int> FetchLiveStream()
-        {
-            return FetchLiveStream(LiveStreamData.Keys.ToList());
-        }
-        /// <summary>
-        /// 根据传入的UID来更新直播列表
-        /// </summary>
-        /// <param name="uids">需要拉取的UID</param>
-        /// <returns>有状态更新的用户</returns>
-        public static List<int> FetchLiveStream(List<int> uids)
-        {
-            if (uids == null || uids.Count == 0) return new();
-            string text = Helper.Post(BaseURL, new {uids}).Result;
-            var json = JsonConvert.DeserializeObject<LiveStreamsModel.Main>(text);
-            List<int> update = new();
+            string text = Helper.Get(BaseUIDQueryURL + UID).Result;
+            var json = JsonConvert.DeserializeObject<LiveStreamsModel.UserInfo_Main>(text);
             if (json.code == 0)
             {
-                foreach (var item in uids)
+                Name = json.data.info.uname;
+                RoomID = json.data.room_id;
+                UserInfo = json.data;
+            }            
+        }
+        public bool FetchLiveStream()
+        {
+            string text = Helper.Get(BaseRoomInfoURL + UID).Result;
+            var json = JsonConvert.DeserializeObject<LiveStreamsModel.RoomInfo_Main>(text);
+            if (json.code == 0)
+            {
+                if (json.data.live_status != StreamingStatus)
                 {
-                    var t = json.data[item.ToString()].ToObject<LiveStreamsModel.RoomInfo>();
-                    if (LiveStreamData.ContainsKey(item))
+                    StreamingStatus = json.data.live_status;
+                    if (Streaming)
                     {
-                        if (t.live_status != LiveStreamData[item].live_status)
-                        {
-                            var room = LiveStreamData[item];
-                            if (t.live_status == 1)
-                            {                            
-                                update.Add(item);
-                                LogHelper.Info("直播状态变更", $"开播了，{room.uname} - {room.title}");
-                            }
-                        }
-
-                        LiveStreamData[item] = t;
-                    }
-                    else
-                    {
-                        LiveStreamData.Add(item, t);
-                    }
-                    LogHelper.Info("直播检查", $"{t.uname}直播状态更新成功");
+                        RoomInfo = json.data;
+                        LogHelper.Info("直播状态变更", $"开播了，{Name} - {RoomInfo.title}");
+                        return true;
+                    }                    
                 }
+                LogHelper.Info("直播检查", $"{Name}直播状态更新成功");
             }
             else
             {
                 Debug.WriteLine(json.message);
             }
-
-            return update;
+            return false;
         }
 
-        public static string DrawLiveStreamPic(int uid)
+        public string DrawLiveStreamPic()
         {
-            if (!LiveStreamData.ContainsKey(uid) || LiveStreamData[uid] == null)
-            {
-                return string.Empty;
-            }
-
-            return DrawLiveStreamPic(LiveStreamData[uid]);
-        }
-
-        public static string DrawLiveStreamPic(LiveStreamsModel.RoomInfo item)
-        {
-            if (item == null) return string.Empty;
+            if (RoomInfo == null || UserInfo == null) return string.Empty;
             using Image<Rgba32> main = new(652, 198, Color.White);
             using Image avatar =
-                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", item.face.GetFileNameFromURL()));
+                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", UserInfo.info.face.GetFileNameFromURL()));
             using Image cover =
-                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", item.cover_from_user.GetFileNameFromURL()));
+                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", RoomInfo.user_cover.GetFileNameFromURL()));
 
             avatar.Mutate(x => x.Resize(48, 48));
             cover.Mutate(x => x.Resize((int) (cover.Width / (cover.Height / 198.0)), 198));
@@ -116,8 +90,8 @@ namespace BilibiliMonitor.BilibiliAPI
             Font bigFont = SystemFonts.CreateFont("Microsoft YaHei", 20);
             TextOptions option = new TextOptions(smallFont);
             PointF point = new(48 + 5, 15);
-            var size = TextMeasurer.Measure(item.uname, option);
-            Info.Mutate(x=>x.DrawText(item.uname, smallFont, Color.Black, point));
+            var size = TextMeasurer.Measure(Name, option);
+            Info.Mutate(x=>x.DrawText(Name, smallFont, Color.Black, point));
             point = new(point.X + size.Width + 5, point.Y);
             Info.Mutate(x=>x.DrawText("开播了", smallFont, Rgba32.ParseHex("#99a2aa"), point));
             point = new(10, point.Y + 60);
@@ -128,39 +102,29 @@ namespace BilibiliMonitor.BilibiliAPI
                 WrappingLength = Info.Width,
                 Origin = point
             };
-            Info.Mutate(x=>x.DrawText(option, item.title, Color.Black));
+            Info.Mutate(x=>x.DrawText(option, RoomInfo.title, Color.Black));
             point = new(10, 178 - 20);
-            Info.Mutate(x=>x.DrawText(item.area_v2_name, smallFont, Rgba32.ParseHex("#99a2aa"), point));
+            Info.Mutate(x=>x.DrawText(RoomInfo.area_name, smallFont, Rgba32.ParseHex("#99a2aa"), point));
             option = new TextOptions(smallFont);
-            size = TextMeasurer.Measure(item.area_v2_name, option);
+            size = TextMeasurer.Measure(RoomInfo.area_name, option);
             point = new(point.X + size.Width, point.Y);
-            Info.Mutate(x=>x.DrawText($" · {Helper.TimeStamp2DateTime(item.live_time):G}", smallFont, Rgba32.ParseHex("#99a2aa"), point));
+            Info.Mutate(x=>x.DrawText($" · {RoomInfo.live_time}", smallFont, Rgba32.ParseHex("#99a2aa"), point));
 
             point = new(cover.Width + 10 ,10);
             main.Mutate(x=>x.DrawImage(Info, (Point) point, 1));
 
             string path = Path.Combine(UpdateChecker.PicPath, "BiliBiliMonitor", "LiveStream");
             Directory.CreateDirectory(path);
-            string filename = $"{item.room_id}.png";
+            string filename = $"{RoomID}.png";
             main.Save(Path.Combine(path, filename));
             return Path.Combine("BiliBiliMonitor", "LiveStream", filename);
         }
 
-        public static void DownloadPics(int uid)
+        public void DownloadPics()
         {
-            if (!LiveStreamData.ContainsKey(uid) || LiveStreamData[uid] == null)
-            {
-                return;
-            }
-
-            DownloadPics(LiveStreamData[uid]);
-        }
-
-        public static void DownloadPics(LiveStreamsModel.RoomInfo item)
-        {
-            if (item == null) return;
-            _ = Helper.DownloadFile(item.cover_from_user, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
-            _ = Helper.DownloadFile(item.face, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
+            if (RoomInfo == null || UserInfo == null) return;
+            _ = Helper.DownloadFile(RoomInfo.user_cover, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
+            _ = Helper.DownloadFile(UserInfo.info.face, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
         }
     }
 }
--- BilibiliMonitor/Models/LiveStreamsModel.cs
+++ BilibiliMonitor/Models/LiveStreamsModel.cs
@@ -4,40 +4,95 @@ namespace BilibiliMonitor.Models
 {
     public class LiveStreamsModel
     {
-
         public class Main
         {
             public int code { get; set; }
             public string msg { get; set; }
             public string message { get; set; }
-            public JObject data { get; set; }
+            public RoomInfo data { get; set; }
         }
-
         public class RoomInfo
         {
-            public string title { get; set; }
-            public int room_id { get; set; }
             public int uid { get; set; }
-            public int online { get; set; }
-            public int live_time { get; set; }
-            public int live_status { get; set; }
+            public int room_id { get; set; }
             public int short_id { get; set; }
-            public int area { get; set; }
-            public string area_name { get; set; }
-            public int area_v2_id { get; set; }
-            public string area_v2_name { get; set; }
-            public string area_v2_parent_name { get; set; }
-            public int area_v2_parent_id { get; set; }
-            public string uname { get; set; }
-            public string face { get; set; }
-            public string tag_name { get; set; }
-            public string tags { get; set; }
-            public string cover_from_user { get; set; }
+            public int attention { get; set; }
+            public int online { get; set; }
+            public bool is_portrait { get; set; }
+            public string description { get; set; }
+            public int live_status { get; set; }
+            public int area_id { get; set; }
+            public int parent_area_id { get; set; }
+            public string parent_area_name { get; set; }
+            public int old_area_id { get; set; }
+            public string background { get; set; }
+            public string title { get; set; }
+            public string user_cover { get; set; }
             public string keyframe { get; set; }
-            public string lock_till { get; set; }
-            public string hidden_till { get; set; }
-            public int broadcast_type { get; set; }
+            public bool is_strict_room { get; set; }
+            public string live_time { get; set; }
+            public string tags { get; set; }
+            public int is_anchor { get; set; }
+            public string room_silent_type { get; set; }
+            public int room_silent_level { get; set; }
+            public int room_silent_second { get; set; }
+            public string area_name { get; set; }
+            public string pendants { get; set; }
+            public string area_pendants { get; set; }
+            public string[] hot_words { get; set; }
+            public int hot_words_status { get; set; }
+            public string verify { get; set; }
+            public New_Pendants new_pendants { get; set; }
+            public string up_session { get; set; }
+            public int pk_status { get; set; }
+            public int pk_id { get; set; }
+            public int battle_id { get; set; }
+            public int allow_change_area_time { get; set; }
+            public int allow_upload_cover_time { get; set; }
+            public Studio_Info studio_info { get; set; }
         }
-
+        public class New_Pendants
+        {
+            public Frame frame { get; set; }
+            public Badge badge { get; set; }
+            public Mobile_Frame mobile_frame { get; set; }
+            public object mobile_badge { get; set; }
+        }
+        public class Frame
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+            public int position { get; set; }
+            public string desc { get; set; }
+            public int area { get; set; }
+            public int area_old { get; set; }
+            public string bg_color { get; set; }
+            public string bg_pic { get; set; }
+            public bool use_old_area { get; set; }
+        }
+        public class Badge
+        {
+            public string name { get; set; }
+            public int position { get; set; }
+            public string value { get; set; }
+            public string desc { get; set; }
+        }
+        public class Mobile_Frame
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+            public int position { get; set; }
+            public string desc { get; set; }
+            public int area { get; set; }
+            public int area_old { get; set; }
+            public string bg_color { get; set; }
+            public string bg_pic { get; set; }
+            public bool use_old_area { get; set; }
+        }
+        public class Studio_Info
+        {
+            public int status { get; set; }
+            public object[] master_list { get; set; }
+        }
     }
 }
