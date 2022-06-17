1ed3a88d09a10d4c8001e9af504d94a2fc569822
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-17 16:23:51 +0800

    添加番剧绘制

--- BilibiliMonitor/BilibiliAPI/Bangumi.cs
+++ BilibiliMonitor/BilibiliAPI/Bangumi.cs
@@ -1,12 +1,130 @@
-﻿using System;
+﻿using BilibiliMonitor.Models;
+using Newtonsoft.Json;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Path = System.IO.Path;
 
 namespace BilibiliMonitor.BilibiliAPI
 {
-    internal class Bangumi
+    public class Bangumi
     {
+        string baseInfoURL = "http://api.bilibili.com/pgc/view/web/season?season_id=";
+        string baseEpURL = "https://api.bilibili.com/pgc/web/season/section?season_id=";
+        public int SeasonID { get; set; }
+        public string Name { get; set; }
+        public int LastID { get; set; }
+        public BangumiModel.DetailInfo BangumiInfo { get; set; }
+        public BangumiModel.Detail_Episode LastEp { get; set; }
+        public Bangumi(int seasonId)
+        {
+            SeasonID = seasonId;
+            FetchInfo();
+        }
+        public bool FetchInfo()
+        {
+            string text = Helper.Get(baseInfoURL + SeasonID).Result;
+            var json = JsonConvert.DeserializeObject<BangumiModel.DetailInfo>(text);
+            if (json.code == 0)
+            {
+                BangumiInfo = json;
+                Name = json.result.season_title;
+                LastID = json.result.episodes.Last().id;
+                LastEp = json.result.episodes.Last();
+                LogHelper.Info("拉取番剧信息", $"{Name} 番剧信息拉取成功");
+                return true;
+            }
+            LogHelper.Info("拉取番剧信息", text, false);
+            return false;
+        }
+        public bool FetchEPDetail()
+        {
+            if (BangumiInfo == null) return false;
+            string text = Helper.Get(baseEpURL + SeasonID).Result;
+            var json = JsonConvert.DeserializeObject<BangumiModel.Main>(text);
+            if(json.code == 0)
+            {
+                if(json.result.main_section.episodes.Last().id != LastID)
+                {
+                    LogHelper.Info("更新番剧信息", "新的剧集出现了");
+                    LastID = json.result.main_section.episodes.Last().id;
+                    FetchInfo();
+                    return true;
+                }
+                return false;
+            }
+            LogHelper.Info("更新番剧信息", text, false);
+            return false;
+        }
+        public void DownloadPic()
+        {
+            if (BangumiInfo == null) return;
+            _ = Helper.DownloadFile(LastEp.cover, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
+            _ = Helper.DownloadFile(BangumiInfo.result.square_cover, Path.Combine(UpdateChecker.BasePath, "tmp")).Result;
+        }
+        public string DrawLastEpPic()
+        {
+            if (BangumiInfo == null || LastEp == null) return string.Empty;
+
+            using Image<Rgba32> main = new(652, 198, Color.White);
+            using Image avatar =
+                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", BangumiInfo.result.square_cover.GetFileNameFromURL()));
+            using Image cover =
+                Image.Load(Path.Combine(UpdateChecker.BasePath, "tmp", LastEp.cover.GetFileNameFromURL()));
+
+            avatar.Mutate(x => x.Resize(48, 48));
+            cover.Mutate(x => x.Resize((int)(cover.Width / (cover.Height / 198.0)), 198));
+
+            main.Mutate(x => x.DrawImage(cover, new Point(0, 0), 1));
+            using Image<Rgba32> Info = new(652 - cover.Width - 20, 178, Color.White);
+
+            using Image<Rgba32> avatarFrame = new(48, 48, new Rgba32(255, 255, 255, 0));
+            IPath circle = new EllipsePolygon(avatarFrame.Width / 2, avatarFrame.Height / 2, avatarFrame.Width / 2);
+            avatarFrame.Mutate(x => x.Fill(new ImageBrush(avatar), circle));
+            Info.Mutate(x => x.DrawImage(avatarFrame, new Point(0, 0), 1));
+
+            Font smallFont = SystemFonts.CreateFont("Microsoft YaHei", 14);
+            Font bigFont = SystemFonts.CreateFont("Microsoft YaHei", 20);
+            TextOptions option = new TextOptions(smallFont);
+            PointF point = new(48 + 5, 15);
+            var size = TextMeasurer.Measure(Name, option);
+            Info.Mutate(x => x.DrawText(Name, smallFont, Color.Black, point));
+            point = new(point.X + size.Width + 5, point.Y);
+            Info.Mutate(x => x.DrawText("更新了", smallFont, Rgba32.ParseHex("#99a2aa"), point));
+            point = new(10, point.Y + 60);
+            option = new TextOptions(bigFont)
+            {
+                TextAlignment = TextAlignment.Start,
+                VerticalAlignment = VerticalAlignment.Center,
+                WrappingLength = Info.Width,
+                Origin = point
+            };
+            Info.Mutate(x => x.DrawText(option, LastEp.long_title, Color.Black));
+            point = new(10, 178 - 20);
+            string epCount = $"第{LastEp.title}话";
+            Info.Mutate(x => x.DrawText(epCount, smallFont, Rgba32.ParseHex("#99a2aa"), point));
+            option = new TextOptions(smallFont);
+            size = TextMeasurer.Measure(epCount, option);
+            point = new(point.X + size.Width, point.Y);
+            Info.Mutate(x => x.DrawText($" · {Helper.TimeStamp2DateTime(LastEp.pub_time):G}", smallFont, Rgba32.ParseHex("#99a2aa"), point));
+            
+            point = new(cover.Width + 10, 10);
+            main.Mutate(x => x.DrawImage(Info, (Point)point, 1));
+
+            string path = Path.Combine(UpdateChecker.PicPath, "BiliBiliMonitor", "Bangumi");
+            Directory.CreateDirectory(path);
+            string filename = $"{LastEp.id}.png";
+            main.Save(Path.Combine(path, filename));
+            return Path.Combine("BiliBiliMonitor", "Bangumi", filename);
+        }
     }
 }
--- BilibiliMonitor/Models/BangumiModel.cs
+++ BilibiliMonitor/Models/BangumiModel.cs
@@ -1,12 +1,381 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BilibiliMonitor.Models
+﻿namespace BilibiliMonitor.Models
 {
-    internal class BangumiModel
+    public class BangumiModel
     {
+        public class Main
+        {
+            public int code { get; set; }
+            public string message { get; set; }
+            public Result result { get; set; }
+        }
+
+        public class Result
+        {
+            public Main_Section main_section { get; set; }
+            public Section[] section { get; set; }
+        }
+
+        public class Main_Section
+        {
+            public Episode[] episodes { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public int type { get; set; }
+        }
+
+        public class Episode
+        {
+            public int aid { get; set; }
+            public string badge { get; set; }
+            public Badge_Info badge_info { get; set; }
+            public int badge_type { get; set; }
+            public int cid { get; set; }
+            public string cover { get; set; }
+            public string from { get; set; }
+            public int id { get; set; }
+            public int is_premiere { get; set; }
+            public string long_title { get; set; }
+            public string share_url { get; set; }
+            public int status { get; set; }
+            public string title { get; set; }
+            public string vid { get; set; }
+        }
+
+        public class Badge_Info
+        {
+            public string bg_color { get; set; }
+            public string bg_color_night { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Section
+        {
+            public Episode1[] episodes { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public int type { get; set; }
+        }
+
+        public class Episode1
+        {
+            public int aid { get; set; }
+            public string badge { get; set; }
+            public Badge_Info1 badge_info { get; set; }
+            public int badge_type { get; set; }
+            public int cid { get; set; }
+            public string cover { get; set; }
+            public string from { get; set; }
+            public int id { get; set; }
+            public int is_premiere { get; set; }
+            public string long_title { get; set; }
+            public string share_url { get; set; }
+            public int status { get; set; }
+            public string title { get; set; }
+            public string vid { get; set; }
+        }
+
+        public class Badge_Info1
+        {
+            public string bg_color { get; set; }
+            public string bg_color_night { get; set; }
+            public string text { get; set; }
+        }
+
+        public class DetailInfo
+        {
+            public int code { get; set; }
+            public string message { get; set; }
+            public Detail_Result result { get; set; }
+        }
+
+        public class Detail_Result
+        {
+            public Activity activity { get; set; }
+            public string alias { get; set; }
+            public Area[] areas { get; set; }
+            public string bkg_cover { get; set; }
+            public string cover { get; set; }
+            public Detail_Episode[] episodes { get; set; }
+            public string evaluate { get; set; }
+            public Freya freya { get; set; }
+            public string jp_title { get; set; }
+            public string link { get; set; }
+            public int media_id { get; set; }
+            public int mode { get; set; }
+            public New_Ep new_ep { get; set; }
+            public Payment payment { get; set; }
+            public Positive positive { get; set; }
+            public Publish publish { get; set; }
+            public Rating rating { get; set; }
+            public string record { get; set; }
+            public Rights rights { get; set; }
+            public int season_id { get; set; }
+            public string season_title { get; set; }
+            public Season[] seasons { get; set; }
+            public Series series { get; set; }
+            public string share_copy { get; set; }
+            public string share_sub_title { get; set; }
+            public string share_url { get; set; }
+            public Show show { get; set; }
+            public int show_season_type { get; set; }
+            public string square_cover { get; set; }
+            public Stat stat { get; set; }
+            public int status { get; set; }
+            public string subtitle { get; set; }
+            public string title { get; set; }
+            public int total { get; set; }
+            public int type { get; set; }
+            public Up_Info up_info { get; set; }
+            public User_Status user_status { get; set; }
+        }
+
+        public class Activity
+        {
+            public string head_bg_url { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+        }
+
+        public class Freya
+        {
+            public string bubble_desc { get; set; }
+            public int bubble_show_cnt { get; set; }
+            public int icon_show { get; set; }
+        }
+
+        public class New_Ep
+        {
+            public string desc { get; set; }
+            public int id { get; set; }
+            public int is_new { get; set; }
+            public string title { get; set; }
+        }
+
+        public class Payment
+        {
+            public int discount { get; set; }
+            public Pay_Type pay_type { get; set; }
+            public string price { get; set; }
+            public string promotion { get; set; }
+            public string tip { get; set; }
+            public int view_start_time { get; set; }
+            public int vip_discount { get; set; }
+            public string vip_first_promotion { get; set; }
+            public string vip_promotion { get; set; }
+        }
+
+        public class Pay_Type
+        {
+            public int allow_discount { get; set; }
+            public int allow_pack { get; set; }
+            public int allow_ticket { get; set; }
+            public int allow_time_limit { get; set; }
+            public int allow_vip_discount { get; set; }
+            public int forbid_bb { get; set; }
+        }
+
+        public class Positive
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+        }
+
+        public class Publish
+        {
+            public int is_finish { get; set; }
+            public int is_started { get; set; }
+            public string pub_time { get; set; }
+            public string pub_time_show { get; set; }
+            public int unknow_pub_date { get; set; }
+            public int weekday { get; set; }
+        }
+
+        public class Rating
+        {
+            public int count { get; set; }
+            public float score { get; set; }
+        }
+
+        public class Rights
+        {
+            public int allow_bp { get; set; }
+            public int allow_bp_rank { get; set; }
+            public int allow_download { get; set; }
+            public int allow_review { get; set; }
+            public int area_limit { get; set; }
+            public int ban_area_show { get; set; }
+            public int can_watch { get; set; }
+            public string copyright { get; set; }
+            public int forbid_pre { get; set; }
+            public int freya_white { get; set; }
+            public int is_cover_show { get; set; }
+            public int is_preview { get; set; }
+            public int only_vip_download { get; set; }
+            public string resource { get; set; }
+            public int watch_platform { get; set; }
+        }
+
+        public class Series
+        {
+            public int series_id { get; set; }
+            public string series_title { get; set; }
+        }
+
+        public class Show
+        {
+            public int wide_screen { get; set; }
+        }
+
+        public class Stat
+        {
+            public int coins { get; set; }
+            public int danmakus { get; set; }
+            public int favorite { get; set; }
+            public int favorites { get; set; }
+            public int likes { get; set; }
+            public int reply { get; set; }
+            public int share { get; set; }
+            public int views { get; set; }
+        }
+
+        public class Up_Info
+        {
+            public string avatar { get; set; }
+            public string avatar_subscript_url { get; set; }
+            public int follower { get; set; }
+            public int is_follow { get; set; }
+            public int mid { get; set; }
+            public string nickname_color { get; set; }
+            public Pendant pendant { get; set; }
+            public int theme_type { get; set; }
+            public string uname { get; set; }
+            public int verify_type { get; set; }
+            public Vip_Label vip_label { get; set; }
+            public int vip_status { get; set; }
+            public int vip_type { get; set; }
+        }
+
+        public class Pendant
+        {
+            public string image { get; set; }
+            public string name { get; set; }
+            public int pid { get; set; }
+        }
+
+        public class Vip_Label
+        {
+            public string bg_color { get; set; }
+            public int bg_style { get; set; }
+            public string border_color { get; set; }
+            public string text { get; set; }
+            public string text_color { get; set; }
+        }
+
+        public class User_Status
+        {
+            public int area_limit { get; set; }
+            public int ban_area_show { get; set; }
+            public int follow { get; set; }
+            public int follow_status { get; set; }
+            public int login { get; set; }
+            public int pay { get; set; }
+            public int pay_pack_paid { get; set; }
+            public int sponsor { get; set; }
+        }
+
+        public class Area
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Detail_Episode
+        {
+            public int aid { get; set; }
+            public string badge { get; set; }
+            public Detail_Badge_Info badge_info { get; set; }
+            public int badge_type { get; set; }
+            public string bvid { get; set; }
+            public int cid { get; set; }
+            public string cover { get; set; }
+            public Dimension dimension { get; set; }
+            public int duration { get; set; }
+            public string from { get; set; }
+            public int id { get; set; }
+            public bool is_view_hide { get; set; }
+            public string link { get; set; }
+            public string long_title { get; set; }
+            public int pub_time { get; set; }
+            public int pv { get; set; }
+            public string release_date { get; set; }
+            public Rights1 rights { get; set; }
+            public string share_copy { get; set; }
+            public string share_url { get; set; }
+            public string short_link { get; set; }
+            public int status { get; set; }
+            public string subtitle { get; set; }
+            public string title { get; set; }
+            public string vid { get; set; }
+        }
+
+        public class Detail_Badge_Info
+        {
+            public string bg_color { get; set; }
+            public string bg_color_night { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Dimension
+        {
+            public int height { get; set; }
+            public int rotate { get; set; }
+            public int width { get; set; }
+        }
+
+        public class Rights1
+        {
+            public int allow_demand { get; set; }
+            public int allow_dm { get; set; }
+            public int allow_download { get; set; }
+            public int area_limit { get; set; }
+        }
+
+        public class Season
+        {
+            public string badge { get; set; }
+            public Detail_Badge_Info1 badge_info { get; set; }
+            public int badge_type { get; set; }
+            public string cover { get; set; }
+            public string horizontal_cover_1610 { get; set; }
+            public string horizontal_cover_169 { get; set; }
+            public int media_id { get; set; }
+            public New_Ep1 new_ep { get; set; }
+            public int season_id { get; set; }
+            public string season_title { get; set; }
+            public int season_type { get; set; }
+            public Stat1 stat { get; set; }
+        }
+
+        public class Detail_Badge_Info1
+        {
+            public string bg_color { get; set; }
+            public string bg_color_night { get; set; }
+            public string text { get; set; }
+        }
+
+        public class New_Ep1
+        {
+            public string cover { get; set; }
+            public int id { get; set; }
+            public string index_show { get; set; }
+        }
+
+        public class Stat1
+        {
+            public int favorites { get; set; }
+            public int series_follow { get; set; }
+            public int views { get; set; }
+        }
+
     }
 }
