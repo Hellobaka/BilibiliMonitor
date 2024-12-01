0aa3bc761ebafd8e41defbf0b42532d7b0615324
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2024-12-01 16:26:39 +0800

    支持动态过滤词以及自定义字体

--- BilibiliMonitor/Config.cs
+++ BilibiliMonitor/Config.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace BilibiliMonitor
@@ -34,15 +35,21 @@ namespace BilibiliMonitor
 
         public static string PicSaveBasePath { get; set; } = "";
 
+        public static string CustomFont { get; set; } = "";
+
+        public static List<string> DynamicFilters { get; set; } = [];
+
         public override void LoadConfig()
         {
             Cookies = GetConfig("Cookies", "");
             RefreshToken = GetConfig("RefreshToken", "");
+            CustomFont = GetConfig("CustomFont", "");
             RefreshInterval = GetConfig("RefreshInterval", 120 * 1000);
             BangumiRetryCount = GetConfig("BangumiRetryCount", 3);
             LiveStreamRetryCount = GetConfig("LiveStreamRetryCount", 3);
             DynamicRetryCount = GetConfig("DynamicRetryCount", 3);
             DebugMode = GetConfig("DebugMode", false);
+            DynamicFilters = GetConfig("DynamicFilters", new List<string>() { "UP主的推荐", "ADDITIONAL_TYPE_GOODS" });
         }
     }
 }
